0219132f4c74be4e86e123500c74c0cfa891710b
theojustinnn <114268813+theojustinnn@users.noreply.github.com>
2023-05-10 08:53:43 +0700

    Update ver. 10/5/23

--- screens/paymentStatus.js
+++ screens/paymentStatus.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Dimensions, StyleSheet, View, Image, ActivityIndicator } from "react-native";
 import { Appbar, Text } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { doc, updateDoc } from "firebase/firestore"; 
+import { db } from "../config/config";
 
 // Get window size for ease
 const win = Dimensions.get("window");
@@ -10,6 +12,32 @@ const win = Dimensions.get("window");
 export default function PaymentStatus() {  
   // Declare navigations
   const navigation = useNavigation();
+  const route = useRoute();
+
+  // Storing passed parameter from previous screen
+  const [kuota, setKuota] = useState(route.params?.kuota);
+  const [ticket, setTicket] = useState(route.params?.ticket);
+  const [namakonser, setNamaKonser] = useState(route.params?.namakonser);
+  const [artis, setArtis] = useState(route.params?.artis);
+  const [tanggal, setTanggal] = useState(route.params?.tanggal);
+  const [kategori, setKategori] = useState(route.params?.kategori);
+
+  // Declare fixed variable
+  const docID = artis + "-" + tanggal + "-" + kategori
+
+  // Using firestore to update
+  useEffect(() => {
+    const updateRef = doc(db, "category", docID)
+
+    if (kuota-ticket < 1) {
+      updateDoc(updateRef, {
+        "status": false
+      })
+    }
+    updateDoc(updateRef, {
+      "kuota" : kuota-ticket
+    })
+  }, [])
 
   // Variables used by default
   const [isWaiting, setWaiting] = useState(true)  // For loading screen
@@ -26,7 +54,8 @@ export default function PaymentStatus() {
       <Appbar.Action icon="close" disabled />}
 
       {!isWaiting &&
-      <Appbar.Action icon="close" onPress={() => navigation.navigate("HomeConcert")} />}
+      // <Appbar.Action icon="close" onPress={() => navigation.navigate("HomeConcert")} />}
+      <Appbar.Action icon="close" onPress={() => navigation.navigate("TicketDetails")} />}
       </Appbar.Header>
       <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
         {isWaiting && 
